c3421a2739e0eddfa75bcda6d3cd174872633c25
santir <santiagoandresrey@gmail.com>
2025-08-22 17:09:14 -0500

    PRIMEROS DATOS

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+        System.out.println("Santiago Andres Rey Meneses");
+        System.out.println("Ingeniero en sistemas");
+
+    }
+}
